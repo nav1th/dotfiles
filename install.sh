e93545fbdfcddd73c45cec1a86d782e64a85ef56
mr-gl17ch <wizard101master2010@Hotmail.co.uk>
2021-10-17 21:22:22 +0100

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+USER=`who | awk '{print $1}'`
+HOME=/home/$USER
 good(){
     printf "\033[92;1mOK: $1\033[0m\n"
 }
@@ -38,30 +41,32 @@ check_internet(){
 install_software(){
     case $distro in
         arch | manjaro)
-            pacman -Syy --noconfirm zsh neovim zsh-syntax-highlighting 2>/dev/null 1>&2
+            pacman -Syy --noconfirm zsh neovim zsh-syntax-highlighting nodejs 2>/dev/null 1>&2
             ;;
         fedora)
             dnf check-update
-            dnf install -y zsh neovim zsh-syntax-highlighting 2>/dev/null 1>&2
+            dnf install -y zsh neovim zsh-syntax-highlighting nodejs 2>/dev/null 1>&2
             ;;
         debian | ubuntu | *)
             apt update 2>/dev/null 1>&2
-            apt install -y zsh neovim 2>/dev/null 1>&2
+            apt install -y zsh neovim zsh-syntax-highlighting nodejs 2>/dev/null 1>&2
             ;;
     esac
-    sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y
-    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-           https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y 2>/dev/null 1>&2
+    mkdir -p $HOME/.config/nvim/
+    curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubUSERcontent.com/junegunn/vim-plug/master/plug.vim 2>/dev/null 1>&2
+    chown $USER "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim
 
 }
 copy_to_conf(){
-    user=`who | awk '{print $1}'`
     tar -xvf conf.tar conffiles 2>/dev/null 1>&2
     cd conffiles 2>/dev/null 
     if [ $? -eq 0 ]; then
-        cp .profile .zshrc /home/$user
-        cp starship.toml /home/$user/.config
-        cp init.vim /home/$user/.config/nvim
+        cp .profile .zshrc $HOME
+        cp starship.toml /home/$USER/.config
+        cp init.vim /home/$USER/.config/nvim
+        chown -R $USER $HOME/.profile $HOME/.zshrc $HOME/.config
         cd ..
         rm -rf conffiles
     else
@@ -69,9 +74,9 @@ copy_to_conf(){
     fi
 }
 
-#check_root
-#check_distro
-#check_internet
-#install_software
+check_root
+check_distro
+check_internet
+install_software
 copy_to_conf
 
