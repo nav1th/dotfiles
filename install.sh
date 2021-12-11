b714ada535fb0fade269220c9d62a8df97b1c0b6
mr-gl17ch <wizard101master2010@hotmail.co.uk>
2021-12-11 19:57:55 +0000

    You know what's new

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
 user=$(who | awk '{print $1}' | head -n1)
 home="/home/$user"
+conf_archive="conffiles.tar.gz" 
+conf_directory="conffiles"
+nvim_dir="$home/.config/nvim"
+zshsynhigh="zsh-syntax-highlighting"
+
 good(){
     printf "\033[92;1mOK: $1\033[0m\n"
 }
@@ -19,17 +25,28 @@ check_root(){
     id=`id -u`
     if [ $id -ne 0 ]; then
         error "you're not root"
-        exit 1
+        return 1
     fi
+    return 0
 }
 check_location(){
     basename="$(dirname $0)"
     realpath="$(realpath $basename)"
     if [$realpath -ne $PWD 2>/dev/null];then
         error "need to be in the script directory to run script"
-        exit 1
+        return 1
     fi
-
+    return 0
+}
+check_internet(){
+    tool=ping
+    msg "checking internet connection..."
+    if ! $tool -c 4 8.8.8.8 >/dev/null 2>&1; then
+        error "not connected to the internet"
+        return 1
+    else 
+	good "internet connection working"
+    fi
 }
 
 check_distro(){
@@ -39,19 +56,18 @@ check_distro(){
         distro=`cat /etc/lsb-release | awk -F '=' '{print $2}' | head -n1`
     distro=`echo ${distro,,}`
     fi
-
 }
 config_packageman(){
     case $distro in
         arch | manjaro)
             pm="pacman" 
             update="-Syy" 
-            install="-S"
+            install="-S --noconfirm"
             ;;
-        fedora)
+        fedora | redhat | centos)
             pm="dnf"
             update="check-update" 
-            install="install"
+            install="install -y"
             ;;
         debian | ubuntu | kali | raspian)
             pm="apt"
@@ -59,9 +75,10 @@ config_packageman(){
             install="install -y"
             ;;
         void)
-            pm="xbps"
-            install="-install"
-            update="-update"
+            pm="xbps-install"
+            install=""
+            update="-install -S "
+	    ;;
         *)
             warn "could not identify distro, running script as if it's debian based"
             if  apt -v 2>/dev/null 1&>2; then
@@ -76,25 +93,19 @@ config_packageman(){
     esac
     return 0
 }
-check_internet(){
-    tool=ping
-    if ! $tool -c 1 8.8.8.8 >/dev/null 2>&1; then
-        error "Not connected to the internet"
-        exit 1
-    fi
-}
 packages_update(){
     msg "updating $distro repositories"
     if $tool $args 2>/dev/null 1>&2; then
         good "repositories updated"
     else
-        error "repositories failed to be updated run $tool $args' to find out why"
+        error "repositories failed to be updated run $pm $update' to find out why"
         return 1
     fi
     return 0
 }
 package_install(){
     msg "installing $1..."
+    sleep 100
         if $pm $install $1 2>/dev/null 1>&2; then
             good "$1 installed"
         else
@@ -105,91 +116,134 @@ package_install(){
 }
 install_nodejs(){
     msg "installing nodejs"
-    if sh -c "$(curl -fsSL https://starship.rs/install.sh)" && package install nodejs 2>/dev/null 1>&2; then
+	case $distro in 
+		fedora | redhat | centos)
+		    sh -c "$(curl -fsSL https://rpm.nodesource.com/setup_lts.x)" 2>/dev/null 1>&2
+		    ;;
+		debian | ubuntu | kali | raspian)
+		    sh -c "$(curl -fsSL https://deb.nodesource.com/setup_lts.x)" 2>/dev/null 1>&2
+		    ;;
+		    *)
+	    esac
+
+    if package install nodejs 2>/dev/null 1>&2; then
         good "nodejs installed"
     else
         warn "nodejs failed to install"
     fi
 }
-install_vim(){
+install_nvim(){
     if ! nvim -v 2>/dev/null 1>&2; then
-        curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage 2>/dev/null
-        chmod 755 nvim.appimage
-        if ! su $user -c "./nvim.appimage" -v 2>/dev/null 1>&2; then
-            ./nvim.appimage --appimage-extract  2>/dev/null 1>&2
-            ./squashfs-root/AppRun --version 2>/dev/null 1>&2
-            mv squashfs-root /opt/nvim && ln -s /opt/nvim/AppRun /usr/bin/nvim 2>/dev/null
-        else
-            mv nvim.appimage /opt/nvim && ln -s /opt/nvim /usr/bin/nvim 
-        fi
+	msg "downloading nvim files..."
+        if curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage 2>/dev/null; then
+	    msg "installing nvim"
+	    chmod 755 nvim.appimage
+	    if ! su $user -c "./nvim.appimage" -v 2>/dev/null 1>&2; then
+		./nvim.appimage --appimage-extract  2>/dev/null 1>&2
+		./squashfs-root/AppRun --version 2>/dev/null 1>&2
+		mv squashfs-root /opt/nvim && ln -s /opt/nvim/AppRun /usr/bin/nvim 2>/dev/null
+	    else
+		mv nvim.appimage /opt/nvim && ln -s /opt/nvim /usr/bin/nvim 
+	    fi
+	    if ! find $home/.local/share/nvim/site/autoload/plug.vim 2>/dev/null 1>&2; then
+		    su $user -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+	   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' 2>/dev/null 1>&2
+	    else 
+		err "failed to download nvim files. are you still connected to the net?"
+    
+	    fi
+	fi
     fi
 }
 install_zsh(){
-    rm  -rf $home/zsh-syntax-highlighting 2>/dev/null
-    if ! ls $home/.zsh-syntax-highlighting 2>/dev/null 1>&2; then
-        msg "installing zsh-syntax-highlighting"
-        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git 2>/dev/null 1>&2
-        mv zsh-syntax-highlighting $home/.zsh-syntax-highlighting
-        chown -R $user $home/.zsh-syntax-highlighting
-        good "zsh-syntax-highlighting installed"
+    package_install zsh 
+    if ! ls $home/.$zshsynhigh 2>/dev/null 1>&2; then
+        msg "downloading $zshsynhigh files. "
+        if git clone https://github.com/zsh-users/$zshsynhigh.git 2>/dev/null 1>&2; then
+	    good "$zshsynhigh files downloaded"
+	    msg "installing $zshsynhigh"
+	    if mv $zshsynhigh $home/.$zshsynhigh 2>/dev/null && chown -R $user $home/.$zshsynhigh 2>/dev/null;then 
+	    	good "$zshsynhigh installed"
+		return 0
+	    else
+		err "$zshsynhigh failed to install"
+		return 1
+	    fi
+	else
+	    err "failed to download $zshsynhigh files. are you still connected to the net?"
+	    return 1
+	fi
     else
-        msg "zsh-syntax-highlighting already installed"
+        msg "$zshsynhigh already installed"
+	return 0
     fi
 }
 install_software(){
     mkdir /opt 2>/dev/null #creates /opt if it hasn't already been created
-    can_use_pm=config_packageman
     packages_update
     install_nodejs
-    install_zsh-syn-high
-    install_vim
+    #install_nvim
     msg "installing starship prompt..."
     if sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y 2>/dev/null 1>&2;then 
         good "starship installed"
     else
         warn "starship failed to install"
     fi
-    curl -fLo "${XDG_DATA_home:-$home/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 2>/dev/null 1>&2
-    chown $user "${XDG_DATA_home:-$home/.local/share}"/nvim/site/autoload/plug.vim
 }
-copy_to_conf(){
+copy_files(){
+	msg "copying $1 to $2"
+        if cp $1 $2 2>/dev/null ;then
+            good "copied $1 to $2"
+    	else
+	    err "failed to copy $1 to $2"
+        fi
+}
+configure(){
+    if zsh -v 2>/dev/null 1>&2;then
+	msg "setting zsh to $user's default shell"
+	usermod --shell /usr/bin/zsh $user 2>/dev/null 1>&2
+    fi
     msg "extracting configuration files"
-    if tar -xvf conf.tar conffiles 2>/dev/null 1>&2;then
+    if tar -xvf $conf_archive $conf_directory 2>/dev/null 1>&2;then
         good "configuration files extracted"
         msg "making configuriation directories"
-        if mkdir -p $home/.config/nvim/ 2>/dev/null 1>&2; then
+        if mkdir -p $nvim_dir 2>/dev/null 1>&2; then
             good "nvim configuation directory made"
         fi
         cd conffiles
-        if cp .profile .zshrc $home;then
-            good "copied .zshrc to $home"
-            good "copied .profile to $home"
-        fi
-        if cp starship.toml $home/.config;then
-            good "copied starship.toml to $home/.config"
-        fi
-        if cp init.vim $home/.config/nvim; then
-            good "copied init.vim to $home/.config/nvim"
-        fi
+	copy_files .zshrc $home
+	copy_files starship.toml $home/.config
+	copy_files .profile $home
+    if nvim -v 2/dev/null 1>&2; then
+	    copy_files init.vim $home/.conifg/nvim
+    fi
+	return 0;
     else
         error "failed to extract configuration files"
         exit 1
     fi
 }
-finishing_touch()(
-    chown -R $user $home/.profile $home/.zshrc $home/.config 2>/dev/null
+cleanup(){
     cd ..
-    rm -rf conffiles zsh-syntax-highlighting 2>/dev/null 
-    usermod --shell /usr/bin/zsh $user 2>/dev/null 1>&2
+    rm -rf $conf_directory $zshsynhigh 2>/dev/null 
     good "done"
     exit 0
-)
-
-check_root 
-check_location
+}
+main(){
+if ! check_root;then
+   exit 1 
+fi
 check_distro
-check_internet
-install_software
-copy_to_conf
-finishing_touch
+if check_location; then
+    config_packageman
+    if check_internet;then
+	install_software
+    fi
+    copy_to_conf
+    cleanup
+#    su $user -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+else 
+    exit 1
+fi
+}
+main
