3fc1c8a6c19d7a7f9dd599007f318ee176648607
mr-gl17ch <wizard101master2010@hotmail.co.uk>
2022-02-07 11:25:36 +0000

    modified:   polybar/config 	modified:   polybar/launch.sh

--- res/polybar/launch.sh
+++ res/polybar/launch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-killall -q i3bar
-killall -q polybar
+pkill i3bar
+pkill polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar main &
+polybar bar1 &
 
 echo "Polybar launched..."
