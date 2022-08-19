c3577cf3a8eed37b3d4edc36947c61030cbd9981
nav1th <navith@keemail.me>
2022-08-19 01:19:32 +0100

    cba to say what i did

--- res/nvim/lua/nav1th/theme/colorscheme.lua
+++ res/nvim/lua/nav1th/theme/colorscheme.lua
@@ -1,8 +1,18 @@
-local colorscheme = "vscode"
+local colorscheme = "carbonfox"
 vim.opt.background = "dark"
 if colorscheme == "vscode" then
     vim.g.vscode_transparent = 1
     vim.g.vscode_style = "dark"
+end
+if colorscheme == "nightfox" or colorscheme == "carbonfox" or colorscheme == "duskfox" then
+    require('nightfox').setup({
+        carbonfox = {
+            bg0 = "#1d1d2b"
+        },
+        options = {
+            transparent = true
+        }
+    })
 end
 --Configure colorscheme
 local status_ok, _ = pcall(vim.cmd, "colorscheme ".. colorscheme)
