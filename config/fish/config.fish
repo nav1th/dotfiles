b60146b8e0ab042b6a5ae18340f2815ef73e54ad
nav1th <navith@keemail.me>
2024-12-26 01:50:52 +0000

    added cool neofetch info when i start shell

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,4 +1,7 @@
 set fish_greeting
+
+neofetch --colors 15 9 9 9 15 15 --ascii_colors 8 9 --block_range
+
 if test -f /usr/local/bin/starship 
     if test -x /usr/local/bin/starship
         starship init fish | source
@@ -15,4 +18,10 @@ if not pgrep --full ssh-agent | string collect >/dev/null
   set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
 end
 
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
+end
 
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/navith/.ghcup/bin $PATH # ghcup-env
