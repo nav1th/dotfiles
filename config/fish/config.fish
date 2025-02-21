1346e5e22b72b81d8ff4d704131df85b790895e7
nav1th <navith@keemail.me>
2025-02-21 01:40:24 +0000

    no redundant ssh stuff and checks if tmux is on before displaying cool neofetch stuff

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,29 +1,18 @@
 set fish_greeting
 
-neofetch --colors 15 9 9 9 15 15 --ascii_colors 8 9 --block_range
+if [ ! $TMUX ]
+    neofetch --colors 15 9 9 9 15 15 --ascii_colors 8 9 --block_range
+end
 
 # Starship prompt
-if test -f /usr/local/bin/starship 
+if test -f /usr/local/bin/starship && test -x /usr/local/bin/starship
     if test -x /usr/local/bin/starship
         starship init fish | source
     end
-else if test -f /usr/bin/starship 
+else if test -f /usr/bin/starship && test -x /usr/bin/starship
     if test -x /usr/bin/starship
         starship init fish | source
     end
 end
 
-if not pgrep --full ssh-agent | string collect >/dev/null
-  eval (ssh-agent -c)
-  set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-  set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-end
 
-# Upon login startx
-if status is-login
-    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
-        exec startx -- -keeptty
-    end
-end
-
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/navith/.ghcup/bin $PATH # ghcup-env
